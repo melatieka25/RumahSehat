edfb629e4368ddb3fc39af6c895b37fdf34e8235
Melati Eka Putri <melatieka25@gmail.com>
2022-11-08 16:06:08 +0700

    Init: Init springboot project

---
+++ RumahSehatWeb/src/main/java/TA_C_SHA_90/RumahSehatWeb/RumahSehatWebApplication.java
@@ -0,0 +1,13 @@
+package TA_C_SHA_90.RumahSehatWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahSehatWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahSehatWebApplication.class, args);
+	}
+
+}
---
+++ RumahSehatWeb/src/test/java/TA_C_SHA_90/RumahSehatWeb/RumahSehatWebApplicationTests.java
@@ -0,0 +1,13 @@
+package TA_C_SHA_90.RumahSehatWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahSehatWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
