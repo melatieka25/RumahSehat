314a3e5d16a80bf73b71f39dfb1aac48aa247676
sl0ck <rastasonna@gmail.com>
2022-11-27 18:56:18 +0700

    Fix: Adjust nginx config

--- RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/repository/AppointmentDb.java
+++ RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/repository/AppointmentDb.java
@@ -4,6 +4,9 @@ import TA_C_SHA_90.RumahSehatAPI.model.AppointmentModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface AppointmentDb extends JpaRepository<AppointmentModel, Long> {
+	Optional<AppointmentModel> findByKode(String kode);
 }
