e47aac00ad6693ab0fd9f7d7b179cc3e64245a6a
sl0ck <rastasonna@gmail.com>
2022-12-03 14:53:24 +0700

    Implement JWT auth on mobile and topup saldo feature

--- RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/restcontroller/PasienRestController.java
+++ RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/restcontroller/PasienRestController.java
@@ -2,6 +2,7 @@ package TA_C_SHA_90.RumahSehatAPI.restcontroller;
 
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.Map;
 
 import javax.validation.Valid;
 
@@ -18,10 +19,12 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.validation.FieldError;
+import org.springframework.security.core.Authentication;
 
 import TA_C_SHA_90.RumahSehatAPI.model.PasienModel;
 import TA_C_SHA_90.RumahSehatAPI.service.PasienRestService;
@@ -91,6 +94,24 @@ public class PasienRestController {
 			return pasienRestService.updatePasien(uuid, pasien);
 		} catch(NoSuchElementException e) {
 			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pasien with UUID " + uuid + " not found.");
+		}
+	}
+	
+	@PostMapping(value = "/pasien/saldo")
+	private ResponseEntity updateSaldo(Authentication authentication, @RequestBody Map<String, String> params) {
+		// Only allow to access one's own saldo
+		String username = params.get("username");
+		String saldo = params.get("saldo");
+		if(!username.equals(authentication.getName())) {
+			return ResponseEntity.status(401).build();
+		}
+		try {
+			PasienModel pasien = pasienRestService.getPasienByUsername(username);
+			pasien.setSaldo(pasien.getSaldo() + Integer.valueOf(saldo));
+			pasienRestService.updatePasien(pasien.getUuid(), pasien);
+			return ResponseEntity.ok("Saldo for pasien " + username + " has been updated successfully");
+		} catch(NoSuchElementException e) {
+			throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Pasien with username " + username + " not found.");
 		}
 	}
 }
