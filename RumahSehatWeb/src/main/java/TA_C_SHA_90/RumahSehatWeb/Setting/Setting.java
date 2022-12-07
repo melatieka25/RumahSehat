e28e858e76eea0339227905f1ae7e38378e18bf4
sl0ck <rastasonna@gmail.com>
2022-12-07 23:08:47 +0700

    Fix: Adjust client settings

--- RumahSehatWeb/src/main/java/TA_C_SHA_90/RumahSehatWeb/Setting/Setting.java
+++ RumahSehatWeb/src/main/java/TA_C_SHA_90/RumahSehatWeb/Setting/Setting.java
@@ -1,7 +1,7 @@
 package TA_C_SHA_90.RumahSehatWeb.Setting;
 
 public class Setting {
-    final public static String CLIENT_BASE_URL = "https://apap-090.cs.ui.ac.id/";
+    final public static String CLIENT_BASE_URL = "https://apap-090.cs.ui.ac.id";
     final public static String CLIENT_LOGIN = CLIENT_BASE_URL + "/validate-ticket";
     final public static String CLIENT_LOGOUT = CLIENT_BASE_URL + "/logout";
     final public static String SERVER_BASE_URL = "https://sso.ui.ac.id/cas2";
