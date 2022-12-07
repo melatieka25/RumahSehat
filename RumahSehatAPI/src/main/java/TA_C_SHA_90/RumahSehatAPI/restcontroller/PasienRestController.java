688fd07677e683d5b4da33a8cd87a9b5fe79152c
Melati Eka Putri <melatieka25@gmail.com>
2022-12-07 18:38:38 +0700

    clean code

--- RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/restcontroller/PasienRestController.java
+++ RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/restcontroller/PasienRestController.java
@@ -6,12 +6,9 @@ import java.util.Map;
 
 import javax.validation.Valid;
 
-import TA_C_SHA_90.RumahSehatAPI.model.LoginModel;
-import TA_C_SHA_90.RumahSehatAPI.model.UserModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,11 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
-import org.springframework.validation.FieldError;
 import org.springframework.security.core.Authentication;
 
 import TA_C_SHA_90.RumahSehatAPI.model.PasienModel;
