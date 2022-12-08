48c2c9f10eadcf06d9f70e494c595d78bb90567c
Melisa Ayu Angelina <melisa.ayu@ui.ac.id>
2022-12-08 21:04:27 +0700

    Fitur 11: Melihat Detail Resep Mobile

--- RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/repository/ResepDb.java
+++ RumahSehatAPI/src/main/java/TA_C_SHA_90/RumahSehatAPI/repository/ResepDb.java
@@ -4,6 +4,9 @@ import TA_C_SHA_90.RumahSehatAPI.model.ResepModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface ResepDb extends JpaRepository<ResepModel, Long> {
+    Optional<ResepModel> findById(Long id);
 }
